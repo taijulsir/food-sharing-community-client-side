31feeaf3762d71554925aeeb622dedda03b8821d
taijulsir <princetaijul123@gmail.com>
2023-11-07 11:12:12 +0600

    update banner sectio in home page

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,84 +1,65 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import './banner.css'
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-// import required modules
-import { Parallax, Pagination, Navigation } from 'swiper/modules';
 const Banner = () => {
     return (
         <div>
-            <Swiper
-                style={{
-                    '--swiper-navigation-color': '#fff',
-                    '--swiper-pagination-color': '#fff',
-                }}
-                speed={600}
-                parallax={true}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Parallax, Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <div
-                    slot="container-start"
-                    className="parallax-bg hero"
-                    style={{
-                        'background-image':
-                            'url(https://i.ibb.co/X7TMrL2/person-serving-another-from-large-pot-shutterstock-11172956638.webp)',
-                    }}
-                    data-swiper-parallax="-23%"
-                ></div>
-                <div className="hero-overlay bg-opacity-90 bg-black"></div>
-                <SwiperSlide >
-                    <div>
-                        <div className="text-2xl md:text-5xl lg:mt-20  font-extrabold text-blue-500" data-swiper-parallax="-300">
-                            Serving with Compassion <br />  and  Love .
-                        </div>
-                        <div data-swiper-parallax="-100">
-                            <p className="text-xl font-bold text-zinc-950 sm:text-white lg:w-2/5  mt-5">
-                                We believe in serving with compassion and ensuring that no one goes hungry. Be a part of our food sharing community and make a positive impact.
-                            </p>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/X2dPv9y/depositphotos-430133314-stock-photo-poor-people-get-free-food.webp)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Empower Our Community with Kindness</h1>
+                                <p className="mb-5">We`re dedicated to making a difference in our community. Join us in our mission to help those in need and share the joy of giving.</p>
+                                <button className="btn btn-primary">Donate Now</button>
+                            </div>
                         </div>
                     </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
 
-                </SwiperSlide>
-                <SwiperSlide>
-                <div>
-                        <div className="text-2xl md:text-5xl lg:mt-20  font-extrabold text-blue-500" data-swiper-parallax="-300">
-                        Empower Our Community  <br />with Kindness  .
-                        </div>
-                        <div data-swiper-parallax="-100">
-                            <p className="text-xl font-bold text-zinc-950 lg:w-2/5  mt-5">
-                            We`re dedicated to making a difference in our community. Join us in our mission to help those in need and share the joy of giving.
-                            </p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                <div>
-                        <div className="text-2xl md:text-5xl lg:mt-20  font-extrabold text-blue-500" data-swiper-parallax="-300">
-                        Share the Blessing  <br />of Food   .
-                        </div>
-                        <div data-swiper-parallax="-100">
-                            <p className="text-xl font-bold text-zinc-950 lg:w-2/5  mt-5">
-                            Every act of kindness matters. Donate food, share your resources, and show you care. Together, we can create a world without hunger.
-                            </p>
+
+                <div id="slide2" className="carousel-item relative w-full">
+                    <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/X7TMrL2/person-serving-another-from-large-pot-shutterstock-11172956638.webp)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Serving with Compassion and Love</h1>
+                                <p className="mb-5">We believe in serving with compassion and ensuring that no one goes hungry. Be a part of our food sharing community and make a positive impact.</p>
+                                <button className="btn btn-primary">Donate Now</button>
+                            </div>
                         </div>
                     </div>
-                </SwiperSlide>
-            </Swiper>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+
+                <div id="slide3" className="carousel-item relative w-full">
+                    <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/yprmcDS/149286970-volunteers-give-food-to-the-poor-donating-food-is-helping-human-friends-in-society.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Share the Blessing of Food</h1>
+                                <p className="mb-5">Every act of kindness matters. Donate food, share your resources, and show you care. Together, we can create a world without hunger.</p>
+                                <button className="btn btn-primary">Donate Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+            </div>
         </div>
-    )
+    );
 };
 
 export default Banner;
